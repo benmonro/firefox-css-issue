b4e03d30a07c19ada2909b69a2eeee2d5df7ddea
Monro, Ben <Ben.Monro@concur.com>
2018-01-24 09:23:45 -0800

    root

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {browserHistory, Route, Router} from 'react-router';
+import {browserHistory, IndexRoute, Route, Router} from 'react-router';
 import PageA from './PageA';
 import PageB from './PageB';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Router history={browserHistory}>
-            <Route path="/a" component={PageA} />
-            <Route path="/b" component={PageB} />
-        </Router>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo}
+                         className="App-logo"
+                         alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <Router history={browserHistory}>
+                    <Route path="/">
+                        <IndexRoute component={PageA}/>
+                        <Route path="/b"
+                               component={PageB}/>
+                    </Route>
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default App;
